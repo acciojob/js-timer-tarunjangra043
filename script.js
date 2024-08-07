c7507aef3200104253c3a35635f0f4a696117dd4
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-07 21:06:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// write js code here if required
+function updateTimer() {
+  var now = new Date();
+
+  var day = now.getDate();
+  var month = now.getMonth() + 1;
+  var year = now.getFullYear();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  var timerString = day + '/' + month + '/' + year + ', ' + hours + ':' + minutes + ':' + seconds;
+
+  document.getElementById('timer').textContent = timerString;
+}
+
+updateTimer();
+
+setInterval(updateTimer, 1000);
